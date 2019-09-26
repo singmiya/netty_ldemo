a0572bf43225453ff47cf15fd76667b5d1d63b1d
singsmiya <jjsomiya@gmail.com>
2019-09-27 00:56:21 +0800

    chapter2 first app

---
+++ src/main/java/com/singsmiya/EchoClient.java
@@ -0,0 +1,52 @@
+package com.singsmiya;
+
+import com.singsmiya.handler.EchoClientHandler;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import jdk.nashorn.internal.runtime.ECMAException;
+
+public class EchoClient {
+    private final String host;
+    private final int port;
+
+    public EchoClient(String host, int port) {
+        this.host = host;
+        this.port = port;
+    }
+
+    public void start() throws Exception {
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            Bootstrap bootstrap = new Bootstrap();
+            bootstrap.group(group)
+                    .channel(NioSocketChannel.class)
+                    .remoteAddress(host, port)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            socketChannel.pipeline().addLast(new EchoClientHandler());
+                        }
+                    });
+            ChannelFuture future = bootstrap.connect().sync();
+            future.channel().closeFuture().sync();
+        } finally {
+            group.shutdownGracefully().sync();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+//        if (args.length != 2) {
+//            System.err.println("Usage: " + EchoClient.class.getSimpleName() + " <host> <port>");
+//            return;
+//        }
+
+        String host = "127.0.0.1";
+        int port = 9999;
+        new EchoClient(host,port).start();
+    }
+}
